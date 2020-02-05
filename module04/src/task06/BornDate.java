28c766cd7d0a2eb8c26aeed2581f639fb37d4fff
DMAkira <DMAkira.dm_akira@mail.ru>
2020-02-05 14:31:38 +0300

    git ignore added

--- module04/src/task06/BornDate.java
+++ module04/src/task06/BornDate.java
@@ -1,4 +1,44 @@
 package task06;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
 public class BornDate {
+    public static void main(String[] args){
+        inputDate();
+
+    }
+
+    public static void inputDate(){
+
+        DateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        Date date = new Date();
+        System.out.println("Вывод " + format.format(date));
+
+
+        System.out.println("Введите год рождения в формате гггг");
+        Scanner scanner = new Scanner(System.in);
+        int yearInput = scanner.nextInt();
+
+        System.out.println("Введите месяц рождения в формате мм");
+        int montInput = scanner.nextInt();
+
+        System.out.println("Введите день рождения в формате дд");
+        int dayInput = scanner.nextInt();
+
+        Calendar birthday = Calendar.getInstance();
+        birthday.set(yearInput,montInput -  1,dayInput);
+
+
+        System.out.println(birthday.getTime());
+        System.out.println(format.format(birthday.getTime()));
+
+        date = birthday.getTime();
+        System.out.println("Вывод " + format.format(date));
+
+    }
 }
--- module04/src/task5/phoneNumber/phoneNumber.java
+++ module04/src/task5/phoneNumber/phoneNumber.java
@@ -8,7 +8,6 @@ public class phoneNumber {
     public static void main(String[] args){
         System.out.println(inputText());
 
-
     }
 
     public static String inputText() {
